23eeb4462f60c1aa0a14e8aeeeaa5518b197f027
sakshamsomra1 <155710532+sakshamsomra1@users.noreply.github.com>
2024-07-08 15:28:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const isValidEmail = emailValidator.validate('example@email.com');
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: true,
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true
 }));

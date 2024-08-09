9f9d946dd9f026fea99f02f10cfaa81e425902d0
sakshamsomra1 <155710532+sakshamsomra1@users.noreply.github.com>
2024-08-09 11:40:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -881,7 +881,7 @@ let istTimeString = `${hours}:${minutes.toString().padStart(2, '0')} ${ampm}`;
                 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 100%;">
                     <tr>
                         <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                            <p style="margin: 0;">New submission from link <a href="http://15.206.1.232/dev/ctlc">Click here</a></p>
+                            <p style="margin: 0;">New submission from link <a href=${clickhere}>Click here</a></p>
                         </td>
                     </tr>
                  <!-- COPY -->
